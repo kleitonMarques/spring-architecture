f36f770c37db618a9186a75b8849056a86717c1e
Kleiton Marques <kleitonm.marques@gmail.com>
2025-08-25 23:08:04 -0300

    Complete architecture layer

---
+++ src/main/java/br/com/kleitonmarques/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,25 @@
+package br.com.kleitonmarques.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo) {
+        return repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo) {
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+}
