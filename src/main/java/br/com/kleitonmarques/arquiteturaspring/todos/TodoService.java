91ebb753d4c9e80b5ca6f2725c29768dbb0d60e2
Kleiton Marques <kleitonm.marques@gmail.com>
2025-08-26 22:38:57 -0300

    Working with components

--- src/main/java/br/com/kleitonmarques/arquiteturaspring/todos/TodoService.java
+++ src/main/java/br/com/kleitonmarques/arquiteturaspring/todos/TodoService.java
@@ -6,17 +6,25 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 
     private TodoRepository repository;
+    private TodoValidator validator;
+    private MailSender mailSender;
 
-    public TodoService(TodoRepository todoRepository) {
-        this.repository = todoRepository;
+
+    public TodoService(TodoRepository repository, TodoValidator validator, MailSender mailSender) {
+        this.repository = repository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
 
     public TodoEntity salvar(TodoEntity novoTodo) {
+        validator.validar(novoTodo);
         return repository.save(novoTodo);
     }
 
     public void atualizarStatus(TodoEntity todo) {
         repository.save(todo);
+        String status = todo.getConcluido() == Boolean.TRUE ? "concluído" : "não concluído";
+        mailSender.enviar("Todo " + todo.getDescricao() + " foi atualizado para " + status);
     }
 
     public TodoEntity buscarPorId(Integer id) {
