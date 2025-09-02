aea8d4bb1994686f2ec8af23ffc8bd8fda73f10d
Kleiton Marques <kleitonm.marques@gmail.com>
2025-09-01 22:37:25 -0300

    Bean Scope

--- src/main/java/br/com/kleitonmarques/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/br/com/kleitonmarques/arquiteturaspring/BeanGerenciado.java
@@ -3,10 +3,20 @@ package br.com.kleitonmarques.arquiteturaspring;
 import br.com.kleitonmarques.arquiteturaspring.todos.TodoEntity;
 import br.com.kleitonmarques.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope("request")
+//@Scope("session")
+//@Scope("application")
 public class BeanGerenciado {
+
+//    private String idUsuarioLogado;
 
     @Autowired
     private TodoValidator validator;
